31b731d3cf8b4b5deb16cd31f99bfaee6b340ae8
Daniel Brice <danielbrice@gmail.com>
2018-06-24 20:49:57 -0700

    finish python implementation

--- pathfinding.py
+++ pathfinding.py
@@ -1,9 +1,20 @@
 import json
+import random
 import sys
 
 def findPaths(start, end, nodes, connected):
+    paths = [[start]]
+    visited = []
+    while (True):
+        if not paths: return paths
+        finished = [ path for path in paths if path[-1] == end ]
+        if finished: return finished
+        visited = [*visited, *[ path[-1] for path in paths ]]
+        paths = [ [*path, node] for path in paths for node in nodes
+                  if node not in visited and connected(node, path[-1]) ]
 
-
-contents = sys.stdin.read()
-matrix = json.loads(contents)
-print(matrix)
+matrix = json.loads(sys.stdin.read())
+nodes = [ n for n in range(len(matrix[0])) ]
+start = random.choice(nodes)
+end = random.choice(nodes)
+print(findPaths(start, end, nodes, lambda n1, n2: matrix[n1][n2] == 1))
